27780a66fbc748e2f3d8d8e7273363a57b81cba4
rkmr039 <rkmr039@gmail.com>
2018-06-07 12:00:15 +0530

    Project created and HelloWorldControler added

---
+++ TraininngSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TraininngSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
